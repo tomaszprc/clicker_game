0fc78a3667724c589edb33348cd5199cbf982527
tprochowicz <tomaszprochowicz@gmail.com>
2020-12-17 21:06:07 +0100

    Created Vue structure app

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app" class="app">
+      <canvas id="canvasGame" width="400" height="480" />
+  </div>
+</template>
+
+<script>
+import goldMine from "./assets/gold-mine.png";
+import grass from "./assets/grass.png";
+
+export default {
+  name: 'App',
+  methods: {
+    generateCanvasImage() {
+      const canvas = document.getElementById('canvasGame');
+      const context  = canvas.getContext('2d');
+      const canvasGoldMine = new Image();
+      const canvasGrass = new Image();
+      canvasGrass.src = grass;
+      canvasGoldMine.src = goldMine;
+      
+      canvasGoldMine.onload = function() {
+        context.drawImage(canvasGoldMine,   canvas.width / 2 - canvasGoldMine.width / 2, canvas.height / 2 - canvasGoldMine.height / 2);
+          context.drawImage(canvasGrass, canvas.width - canvasGrass.width - 0, canvas.height - canvasGrass.height - 0);
+      }
+    }
+  },
+  mounted: function() {
+    this.generateCanvasImage();
+
+  },
+  components: {
+  }
+}
+</script>
+
+
+<style lang="scss">
+  .app {
+    width: 853px;
+    height: 480px;
+    margin:0 auto;
+    
+  }
+</style>
